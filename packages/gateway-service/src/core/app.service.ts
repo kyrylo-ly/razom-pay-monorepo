c0125c7d3c2f5a18f673e379a4b1b771617ad826
Kyrylo <kyrylo.lytvishko@gmail.com>
2026-06-24 20:28:26 +0300

    feat:enable cors, add logs

---
+++ packages/gateway-service/src/core/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class AppService {
+	getHello() {
+		return { message: 'Welcome to Razom Pay API' }
+	}
+
+	health() {
+		return {
+			status: 'ok',
+			timeStamp: new Date().toISOString()
+		}
+	}
+}
--- packages/gateway-service/src/main.ts
+++ packages/gateway-service/src/main.ts
@@ -1,9 +1,26 @@
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 
-import { AppModule } from './app.module'
+import { AppModule } from './core/app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(process.env.PORT ?? 3000)
+
+	const config = app.get(ConfigService)
+	const logger = new Logger()
+
+	app.enableCors({
+		origin: config.getOrThrow<string>('HTTP_CORS').split(','),
+		credentials: true
+	})
+
+	const port = config.getOrThrow<number>('HTTP_PORT')
+	const host = config.getOrThrow<string>('HTTP_HOST')
+
+	await app.listen(port)
+
+	logger.log(`Gateway started: ${host}`)
+	logger.log(`Swagger: ${host}/docs`)
 }
 bootstrap()
