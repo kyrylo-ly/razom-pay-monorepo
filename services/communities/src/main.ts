ca47535dd20334d25e3c227deaffedc6c6cc4d70
Kyrylo <kyrylo.lytvishko@gmail.com>
2026-06-26 09:58:48 +0300

    feat: connect grpc microservice communities

--- services/communities/src/main.ts
+++ services/communities/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ConfigService } from "@nestjs/config";
+import { Transport, type MicroserviceOptions } from "@nestjs/microservices";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
 
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: "communities.v1",
+      protoPath: "COMMUNITES",
+      loader: {
+        keepCase: false,
+        longs: String,
+        enums: Number,
+        defaults: true,
+        oneofs: true,
+      },
+    },
+  });
   await app.startAllMicroservices();
   await app.listen(9104);
 }
